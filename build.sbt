705bc1965be5750852f98993cb86910f2b76f8ba
Nixonite <programminglinguist@gmail.com>
2019-04-28 20:37:46 -0700

    init commit

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "norepeat"
+
+version := "0.1"
+
+scalaVersion := "2.12.5"
+
+libraryDependencies +=  "com.typesafe.akka" %% "akka-actor" % "2.5.22"
+libraryDependencies += "com.github.alexandrnikitin" %% "bloom-filter" % "latest.release"
---
+++ src/main/scala/com/norepeat/norepeat.scala
@@ -0,0 +1,43 @@
+package com.norepeat
+
+import akka.actor.Actor
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.util.Timeout
+
+import scala.concurrent.{Await, duration, Future}
+import scala.concurrent.duration._
+import akka.pattern.ask
+import bloomfilter.mutable.BloomFilter
+
+final case class Message(val msg: String)
+final case class CheckMessage(val msg: String)
+
+class BloomFilterActor extends Actor {
+
+  val expectedMessages = 1000000
+  val falsePositiveRate = 0.00001
+  val bf = BloomFilter[String](expectedMessages, falsePositiveRate)
+
+  override def receive: Receive = {
+    case Message(msg) => bf.add(msg)
+    case CheckMessage(msg) => sender() ! bf.mightContain(msg)
+  }
+}
+
+object NoRepeat extends App {
+
+  val system = ActorSystem("Chats")
+  // default Actor constructor
+
+  val bloomActor = system.actorOf(Props[BloomFilterActor], name="bloomactor")
+
+  implicit val timeout = Timeout(30 seconds)
+  bloomActor ! Message("hi")
+  bloomActor ! Message("hi")
+  val future: Future[Boolean] = ask(bloomActor, CheckMessage("hi")).mapTo[Boolean]
+  val res = Await.result(future, timeout.duration)
+  println(res)
+  system.terminate()
+}
